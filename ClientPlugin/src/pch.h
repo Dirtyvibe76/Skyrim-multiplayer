effd07339b8f6a7410d341818c54b1a0487f1b32
Dirtyvibe76 <aronsilvernailj@gmail.com>
2026-09-03 02:07:54 -0400

    Fix CommonLib/Windows include order in client PCH

--- ClientPlugin/src/pch.h
+++ ClientPlugin/src/pch.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include "RE/Skyrim.h"
+#include "SKSE/SKSE.h"
+
 #ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
 #endif
@@ -10,9 +13,6 @@
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include <windows.h>
-
-#include "RE/Skyrim.h"
-#include "SKSE/SKSE.h"
 
 #include <atomic>
 #include <chrono>
