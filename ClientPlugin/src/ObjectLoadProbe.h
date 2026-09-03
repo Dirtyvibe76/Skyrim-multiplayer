182499d0da7184b6de23534266ad9eb91b19cfe9
Dirtyvibe76 <aronsilvernailj@gmail.com>
2026-09-02 20:26:23 -0400

    Queue object load events for safe update-thread handoff

--- ClientPlugin/src/ObjectLoadProbe.h
+++ ClientPlugin/src/ObjectLoadProbe.h
@@ -1,12 +1,22 @@
 #pragma once
 
+#include <cstdint>
+#include <vector>
+
 namespace SkyrimMP
 {
+    struct ObjectLoadEventRecord
+    {
+        std::uint32_t formId{};
+        bool loaded{};
+    };
+
     class ObjectLoadProbe final : public RE::BSTEventSink<RE::TESObjectLoadedEvent>
     {
     public:
         static ObjectLoadProbe* GetSingleton();
         static bool Install();
+        static std::vector<ObjectLoadEventRecord> DrainPending();
 
         RE::BSEventNotifyControl ProcessEvent(
             const RE::TESObjectLoadedEvent* a_event,
