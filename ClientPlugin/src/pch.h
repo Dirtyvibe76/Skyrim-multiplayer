f1fa7d4bbec2a762b9049fe2bb9cfcaed08e3ebd
Dirtyvibe76 <aronsilvernailj@gmail.com>
2026-09-03 02:09:30 -0400

    Fix CommonLib PCH Windows header conflict

--- ClientPlugin/src/pch.h
+++ ClientPlugin/src/pch.h
@@ -3,17 +3,6 @@
 #include "RE/Skyrim.h"
 #include "SKSE/SKSE.h"
 
-#ifndef WIN32_LEAN_AND_MEAN
-#define WIN32_LEAN_AND_MEAN
-#endif
-#ifndef NOMINMAX
-#define NOMINMAX
-#endif
-
-#include <winsock2.h>
-#include <ws2tcpip.h>
-#include <windows.h>
-
 #include <atomic>
 #include <chrono>
 #include <cstdint>
