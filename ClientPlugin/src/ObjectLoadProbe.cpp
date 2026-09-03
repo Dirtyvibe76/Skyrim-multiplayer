df8c6d9e0c958dc4d4e92ee81c65f62cd77d6e2d
Dirtyvibe76 <aronsilvernailj@gmail.com>
2026-09-02 20:39:52 -0400

    Label object event queue as RE-0.4f

--- ClientPlugin/src/ObjectLoadProbe.cpp
+++ ClientPlugin/src/ObjectLoadProbe.cpp
@@ -23,12 +23,12 @@ namespace SkyrimMP
     {
         auto* sourceHolder = RE::ScriptEventSourceHolder::GetSingleton();
         if (!sourceHolder) {
-            logs::critical("[RE-0.4e] ScriptEventSourceHolder unavailable");
+            logs::critical("[RE-0.4f] ScriptEventSourceHolder unavailable");
             return false;
         }
 
         sourceHolder->AddEventSink<RE::TESObjectLoadedEvent>(GetSingleton());
-        logs::info("[RE-0.4e] TESObjectLoadedEvent sink installed");
+        logs::info("[RE-0.4f] TESObjectLoadedEvent sink installed");
         return true;
     }
 
