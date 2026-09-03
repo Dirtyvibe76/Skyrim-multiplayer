442104df8b20734d2ea52c21cba1de4dbde59941
Dirtyvibe76 <aronsilvernailj@gmail.com>
2026-09-02 20:50:31 -0400

    Bump object load probe to RE-0.4g

--- ClientPlugin/src/ObjectLoadProbe.cpp
+++ ClientPlugin/src/ObjectLoadProbe.cpp
@@ -23,12 +23,12 @@ namespace SkyrimMP
     {
         auto* sourceHolder = RE::ScriptEventSourceHolder::GetSingleton();
         if (!sourceHolder) {
-            logs::critical("[RE-0.4f] ScriptEventSourceHolder unavailable");
+            logs::critical("[RE-0.4g] ScriptEventSourceHolder unavailable");
             return false;
         }
 
         sourceHolder->AddEventSink<RE::TESObjectLoadedEvent>(GetSingleton());
-        logs::info("[RE-0.4f] TESObjectLoadedEvent sink installed");
+        logs::info("[RE-0.4g] TESObjectLoadedEvent sink installed");
         return true;
     }
 
