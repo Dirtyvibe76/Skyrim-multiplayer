644c02c5bf2299133cefb0448f2c50009ac10f72
Dirtyvibe76 <aronsilvernailj@gmail.com>
2026-09-02 23:38:10 -0400

    Add canonical winning-record database

---
+++ Server/src/CanonicalRecordDatabase.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "BethesdaRecordScanner.h"
+
+#include <cstdint>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+namespace SkyrimMP::Server
+{
+    struct CanonicalRecordKey
+    {
+        FormNamespaceKind kind{ FormNamespaceKind::Full };
+        std::uint32_t namespaceIndex{};
+        std::uint32_t localId{};
+
+        bool operator==(const CanonicalRecordKey&) const = default;
+    };
+
+    struct CanonicalRecordKeyHash
+    {
+        std::size_t operator()(const CanonicalRecordKey& a_key) const noexcept;
+    };
+
+    struct WinningRecord
+    {
+        CanonicalRecordKey key;
+        std::string type;
+        std::uint32_t recordFlags{};
+        std::uint32_t sourceStackIndex{};
+        std::string sourcePlugin;
+    };
+
+    struct CanonicalRecordDatabase
+    {
+        std::uint64_t scannedRecords{};
+        std::uint64_t winningRecords{};
+        std::uint64_t overrideCount{};
+        std::uint64_t typeMismatchOverrides{};
+        std::unordered_map<CanonicalRecordKey, WinningRecord, CanonicalRecordKeyHash> winners;
+    };
+
+    CanonicalRecordDatabase BuildCanonicalRecordDatabase(
+        const std::vector<PluginStackEntry>& a_stack,
+        const std::vector<PluginNamespace>& a_namespaces);
+}
