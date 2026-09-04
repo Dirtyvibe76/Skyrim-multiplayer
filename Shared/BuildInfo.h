ba754c6ea8378b105e8fecace1d5ea73a4965a33
Dirtyvibe76 <aronsilvernailj@gmail.com>
2026-09-04 16:32:12 -0400

    Publish appearance protocol identity

--- Shared/BuildInfo.h
+++ Shared/BuildInfo.h
@@ -7,5 +7,5 @@ namespace SkyrimMP::BuildInfo
     inline constexpr auto kVersion = "0.1.0-alpha.3";
     inline constexpr auto kChannel = "re-0.1-runtime-probe";
     inline constexpr std::uint16_t kWireProtocol = 2;
-    inline constexpr std::uint16_t kReplicationProtocol = 8;
+    inline constexpr std::uint16_t kReplicationProtocol = 9;
 }
