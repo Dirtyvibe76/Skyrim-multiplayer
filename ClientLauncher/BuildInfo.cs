f15e050769205db00c49d269be748f218312214c
Dirtyvibe76 <aronsilvernailj@gmail.com>
2026-09-04 16:32:23 -0400

    Match launcher to appearance protocol

--- ClientLauncher/BuildInfo.cs
+++ ClientLauncher/BuildInfo.cs
@@ -5,7 +5,7 @@ internal static class BuildInfo
     public const string Version = "0.1.0-alpha.3";
     public const string Channel = "re-0.1-runtime-probe";
     public const ushort WireProtocol = 2;
-    public const ushort ReplicationProtocol = 8;
+    public const ushort ReplicationProtocol = 9;
 
     public static string Display => $"{Version} | wire {WireProtocol} | replication {ReplicationProtocol}";
 }
