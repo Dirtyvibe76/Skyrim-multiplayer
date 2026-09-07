5c8f2e03604abcda141675e0af1fdb131d0305a2
Dirtyvibe76 <aronsilvernailj@gmail.com>
2026-09-07 13:39:08 -0400

    Show unique launcher build identity

--- ClientLauncher/BuildInfo.cs
+++ ClientLauncher/BuildInfo.cs
@@ -1,11 +1,17 @@
+using System.Reflection;
+
 namespace SkyrimMPLauncher;
 
 internal static class BuildInfo
 {
-    public const string Version = "0.1.0-alpha.4";
+    public const string ReleaseVersion = "0.1.0-alpha.4";
     public const string Channel = "re-0.1-runtime-probe";
     public const ushort WireProtocol = 2;
     public const ushort ReplicationProtocol = 10;
+
+    public static string Version =>
+        typeof(BuildInfo).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion
+        ?? ReleaseVersion;
 
     public static string Display => $"{Version} | wire {WireProtocol} | replication {ReplicationProtocol}";
 }
